62aa0ed32a37a1d02a34f1c668e0f8eac3999943
MyGitHubBlueberry <namespaceblueberry@gmail.com>
2025-12-14 21:40:42 +0100

    feat: added tab background

--- lua/plugins/lazy-plugins.lua
+++ lua/plugins/lazy-plugins.lua
@@ -1,49 +1,56 @@
 require('lazy').setup({
-    { 'lewis6991/gitsigns.nvim',           opts = {} },
-    { 'brenoprata10/nvim-highlight-colors',       config = function()
-        require ('nvim-highlight-colors').setup({})
-    end },
-    { 'Leviathenn/nvim-transparent',        opts = {
-        enable = true,
-        extra_groups = "all",
-        exclude = {
-            "Pmenu",
-            "CmpFloatScroll",
-            "PmenuSel",
-            "PmenuKindSel",
-            "PmenuExtraSel",
-            "PmenuKind",
-            "PmenuExtra",
-            "PmenuSbar",
-            "PmenuThumb",
-            "Scrollbar",
-            "ModeMsg",
-            "DevIconOrgMode",
-            "StatusLine",
-            "StatusLineNC",
-            "StatusLineTerm",
-            "StatusLineTermNC",
-            "MiniStatuslineModeNormal",
-            "MiniStatuslineModeInsert",
-            "MiniStatuslineModeVisual",
-            "MiniStatuslineModeReplace",
-            "MiniStatuslineModeCommand",
-            "MiniStatuslineModeOther",
-            "MiniStatuslineDevinfo",
-            "MiniStatuslineFilename",
-            "MiniStatuslineFileinfo",
-            "MiniStatuslineInactive",
-            "Visual",
-            "VisualNC",
-            "DiffAdd",
-            "DiffChange",
-            "DiffDelete",
-            "DiffText",
-            "ErrorMsg",
-            "Error",
-            "NvimInternalError",
-        },
-    }},
+    { 'lewis6991/gitsigns.nvim', opts = {} },
+    {
+        'brenoprata10/nvim-highlight-colors',
+        config = function()
+            require('nvim-highlight-colors').setup({})
+        end
+    },
+    {
+        'Leviathenn/nvim-transparent',
+        opts = {
+            enable = true,
+            extra_groups = "all",
+            exclude = {
+                "Pmenu",
+                "CmpFloatScroll",
+                "PmenuSel",
+                "PmenuKindSel",
+                "PmenuExtraSel",
+                "PmenuKind",
+                "PmenuExtra",
+                "PmenuSbar",
+                "PmenuThumb",
+                "Scrollbar",
+                "ModeMsg",
+                "DevIconOrgMode",
+                "StatusLine",
+                "StatusLineNC",
+                "StatusLineTerm",
+                "StatusLineTermNC",
+                "MiniStatuslineModeNormal",
+                "MiniStatuslineModeInsert",
+                "MiniStatuslineModeVisual",
+                "MiniStatuslineModeReplace",
+                "MiniStatuslineModeCommand",
+                "MiniStatuslineModeOther",
+                "MiniStatuslineDevinfo",
+                "MiniStatuslineFilename",
+                "MiniStatuslineFileinfo",
+                "MiniStatuslineInactive",
+                "Visual",
+                "TabLineSel",
+                "VisualNC",
+                "DiffAdd",
+                "DiffChange",
+                "DiffDelete",
+                "DiffText",
+                "ErrorMsg",
+                "Error",
+                "NvimInternalError",
+            },
+        }
+    },
     require 'plugins/config/telescope',
     require 'plugins/config/which-key',
     require 'plugins/config/cmp',
@@ -52,7 +59,7 @@ require('lazy').setup({
     require 'plugins/config/treesitter',
     {
         'deviantfero/wpgtk.vim',
-        priority = 1000, -- Make sure to load this before all the other start plugins.
+        priority = 1000,         -- Make sure to load this before all the other start plugins.
         config = function()
             vim.cmd.colorscheme('wpgtk')
         end
@@ -60,17 +67,17 @@ require('lazy').setup({
     'mbbill/undotree',
     'tpope/vim-fugitive',
     -- --for yuck like in eww
-    -- { 'gpanders/nvim-parinfer' }, 
-    -- { 'neodev.nvim' }, 
+    -- { 'gpanders/nvim-parinfer' },
+    -- { 'neodev.nvim' },
     -- -- for non declarative systems
-    -- { "williamboman/mason.nvim" }, 
+    -- { "williamboman/mason.nvim" },
     -- -- for debugger
     -- { "mfussenegger/nvim-dap" },
     -- { "rcarriga/nvim-dap-ui",              dependencies = {
     --     "mfussenegger/nvim-dap",
     --     "nvim-neotest/nvim-nio",
     -- } },
-    -- 
+    --
     -- require 'plugins/config/harpoon',
     -- require 'plugins/config/debugger',
     --
